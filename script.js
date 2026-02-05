a939eea3f127fda9543364c3bcedeb656e1fb686
Kenneth Sweet <kenny.sweet@hotmail.com>
2026-02-05 15:26:16 -0500

    Materials and GUI stuff a few simple objects
    Added textures, GUI controls, and new 3D objects.

--- script.js
+++ script.js
@@ -1,37 +1,124 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
 
-const scene = new THREE.Scene()
+//textures
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('wavykeyboard.JPG')
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-
-scene.add(mesh)
-const sizes = {
-    width: 800,
-    height: 600
-}
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-console.log(mesh.position.distanceTo(camera.position))
-
-camera.position.x = 3
-
-camera.position.z = 5
-
-scene.add(camera)
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+const gui = new dat.GUI()
+const debugObject = {}
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-camera.lookAt(new THREE.Vector3(0, - 1, 0))
-// ...
 
-// Renderer
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const material = new THREE.MeshBasicMaterial()
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = - 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
+
+texture.colorSpace = THREE.SRGBColorSpace
+material.color = new THREE.Color('#1f3576')
+
+
+gui.add(material, 'wireframe')
+gui
+    .addColor(material, 'color')
+    .onChange(() =>
+    {
+        console.log(value.getHexString())
+    })
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+debugObject.spin = () =>
+{
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
+}
+window.addEventListener('resize',() => {
+    //update screen size
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width,sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+})
+window.addEventListener('dblclick',() => {
+    if(!document.fullscreenElement){
+        canvas.requestFullscreen()
+    }
+    else{
+        document.exitFullscreen()
+        console.log('leave fullscreen')
+    }
+})
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
-renderer.render(scene, camera)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
